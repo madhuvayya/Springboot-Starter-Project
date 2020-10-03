48187e220f49e81ccbf6ce5bdf740a417e47f1b5
madhuvayya <madhu.vayya@gmail.com>
2020-10-03 22:19:32 +0530

    Added code to add new topic by passing request body with topics uri

--- src/main/java/com/bridgelabz/springbootstarter/topic/TopicService.java
+++ src/main/java/com/bridgelabz/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics =  Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic(1,"Java","Most widely user backend programming language"),
 			new Topic(2,"Sql","Sql is the most idely used language to interact with database"),
 			new Topic(3,"Frontend","Html,css and Javascript")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -20,6 +21,10 @@ public class TopicService {
 
 	public Topic getTopicById(int id) {
 		return topics.stream().filter(topic -> topic.getId() == id).findAny().get();
+	}
+
+	public boolean addTopic(Topic topic) {
+		return topics.add(topic);
 	}
 
 }
--- src/main/java/com/bridgelabz/springbootstarter/topic/TopicsController.java
+++ src/main/java/com/bridgelabz/springbootstarter/topic/TopicsController.java
@@ -3,8 +3,10 @@ package com.bridgelabz.springbootstarter.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,18 +15,26 @@ public class TopicsController {
 	@Autowired
 	private TopicService topicService;
 
-	@GetMapping("/topics")
+	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
 	}
 	
-	@GetMapping("/topic/{id}")
+	@RequestMapping("/topic/{id}")
 	public Topic getTopic(@PathVariable("id") int id) {
 		Topic topic = topicService.getTopicById(id);
 		if(!topic.equals(null)) {
 			return topic;
 		}
 		return null;
+	}
+	
+	@RequestMapping(value="/topics",method = RequestMethod.POST)
+	public String addTopic(@RequestBody Topic topic) {
+		if(topicService.addTopic(topic)) {
+			return "Topic added";
+		}
+		return "Failed to add topic";
 	}
 
 }
